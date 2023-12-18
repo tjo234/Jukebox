31fd0ee6abcf5abf3c7107f2b45629c130907a7d
tjo234 <tjo234@gmail.com>
2023-12-17 21:18:17 -0500

    Update installer

--- install.sh
+++ install.sh
@@ -17,65 +17,60 @@ sudo -i
 
 # Update APT to latest version
 echo "JUKEBOX - Update APT installer to latest version..."
-sudo apt update
+apt update
 
 echo "JUKEBOX - Upgrade any outdated packages..."
-sudo apt -y upgrade
+apt -y upgrade
 
 echo "JUKEBOX - Remove any unused packages..."
-sudo apt -y autoremove
+apt -y autoremove
 
 # Install Apache Web Server
 echo "JUKEBOX - Install Apache2 Web Server..."
-sudo apt -y install apache2
-
-# Replace Apache Config file
-sudo cp /var/www/Jukebox/jukebox/config/jukebox.conf /etc/apache2/sites-available/jukebox.conf
-sudo a2ensite jukebox
+apt -y install apache2
 
 # Delete/replace application folder
 echo "JUKEBOX - Delete/create Jukebox application folder..."
-sudo rm -R /var/www/Jukebox/
-cd /var/www
+rm -R /var/www/Jukebox
 
 # Download latest application code
 echo "JUKEBOX - Downloading code from GitHub..."
-sudo git clone https://github.com/tjo234/Jukebox.git
+git clone https://github.com/tjo234/Jukebox.git /var/www/Jukebox
 
 # Create VirtualEnv
 echo "JUKEBOX - Setup Python Virtual Environment..."
 cd /var/www/Jukebox
-sudo python3 -m venv .env
+python3 -m venv .env
 source .env/bin/activate
 
 # Install dependencies into VirtualEnv
 echo "JUKEBOX - Install dependencies..."
-sudo pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Create database 
 echo "JUKEBOX - Create Database folder and set permissions..."
-sudo mkdir /var/database
-sudo chown -R www-data:www-data /var/database
-sudo chmod -R u+w /var/database
+mkdir /var/database
+chown -R www-data:www-data /var/database
+chmod -R u+w /var/database
+
+# Replace Apache Config file
+cp /var/www/Jukebox/jukebox/config/jukebox.conf /etc/apache2/sites-available/jukebox.conf
+a2ensite jukebox
 
 # Restart Apache Server
 echo "JUKEBOX - Restart Apache Server..."
-sudo systemctl reload apache2
+systemctl reload apache2fF
 
 # Add latest updates for MPD on RaspberryPi OS Bullseye
 # More info: https://kaliko.me/debian/
 echo "JUKEBOX - Download latest MPD packages from custom repo..."
-sudo wget -O /tmp/kaliko-keyring.deb https://deb.kaliko.me/kaliko-archive-keyring.deb
-sudo apt install /tmp/kaliko-keyring.deb
+wget -O /tmp/kaliko-keyring.deb https://deb.kaliko.me/kaliko-archive-keyring.deb
+apt install /tmp/kaliko-keyring.deb
 distribution=$(lsb_release -si|tr "[:upper:]" "[:lower:]")
 release=$(lsb_release -sc)
 echo "deb [signed-by=/usr/share/keyrings/deb.kaliko.me.gpg] \
   https://deb.kaliko.me/${distribution}-backports/ ${release}-backports main" \
   > /etc/apt/sources.list.d/deb.kaliko.me.list
-
-# Update APT Package
-echo "JUKEBOX - Updating MPD package..."
-sudo apt update
 
 # Exit SuperUser Mode
 exit
@@ -84,13 +79,19 @@ exit
 echo "JUKEBOX - Installing MPD..."
 sudo apt -y install mpd
 
+# Back to SuperUse
+sudo -i
+
 # Update MPD config file
 echo "JUKEBOX - Update config file..."
-sudo cp /var/www/Jukebox/jukebox/config/mpd.conf /etc/mpd.conf
+sudo cp /var/www/Jukebox/jukebox/config/mpd.conf ~/.config/mpd/mpd.conf
 
 # Copy test file into library
-sudo cp /var/www/Jukebox/jukebox/audio/test.flac /var/lib/mpd/music/
-# Update MPD config file
+cp /var/www/Jukebox/jukebox/audio/test.flac ~/jukebox
 
+# Update MPD config file
 echo "JUKEBOX - Reload Music Player Daemon..."
 sudo systemctl reload apache2
+
+# Exit SuperUser Mode
+exit
