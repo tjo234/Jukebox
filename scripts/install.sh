640010c2fb8ea08201b56b2079324a72846808f0
tjo234 <tjo234@gmail.com>
2023-12-20 22:30:32 -0500

    Tweak update script

--- scripts/install.sh
+++ scripts/install.sh
@@ -27,7 +27,7 @@ apt -y autoremove
 # Install Apache Web Server
 echo -e "\n\n***************\n\nJUKEBOX - Install Apache2 WSGI...\n\n"
 apt -y install apache2
-apt -y install libapache2-mod-wsgi-py3
+apt -y install apache2-dev
 
 # Delete/replace application folder
 echo -e "\n\n***************\n\nJUKEBOX - Delete/create Jukebox application folder...\n\n"
@@ -40,15 +40,16 @@ git clone https://github.com/tjo234/Jukebox.git /var/www/Jukebox
 # Create VirtualEnv
 echo -e "\n\n***************\n\nJUKEBOX - Setup Python Virtual Environment...\n\n"
 cd /var/www/Jukebox
-python3 -m venv .env
-source .env/bin/activate
 
 # Install dependencies into VirtualEnv
 echo -e "\n\n***************\n\nJUKEBOX - Install dependencies...\n\n"
 pip install -r requirements.txt
 
+# Install mod_wsgi into VirtualEnv
+pip install mod_wsgi
+
 # Replace Apache Config file
-cp /var/www/Jukebox/setup/jukebox.conf /etc/apache2/sites-available/jukebox.conf
+cp /var/www/Jukebox/setup/apache.conf /etc/apache2/sites-available/jukebox.conf
 a2ensite jukebox
 
 # Remove default config
@@ -79,7 +80,10 @@ exit
 echo -e "\n\n***************\n\nJUKEBOX - Installing MPD...\n\n"
 sudo apt -y install mpd/bullseye-backports
 
-# Back to SuperUse
+# Run MPD server
+mpd
+
+# Back to SuperUser
 sudo -i
 
 # Update MPD config file
@@ -92,25 +96,32 @@ sudo systemctl restart mpd
 
 # Create database 
 echo -e "\n\n***************\n\nJUKEBOX - Create Database folder and set permissions...\n\n"
-mkdir /var/database
-chown -R www-data:www-data /var/database
-chmod -R u+w /var/database
+# mkdir /var/database
+# chown -R www-data:www-data /var/database
+# chmod -R u+w /var/database
 
-# Add Jukebox folder
-mkdir /jukebox
-chmod 777 /jukebox
-cp /var/www/Jukebox/setup/intro.mov /jukebox/
-cp /var/www/Jukebox/setup/desktop.png /jukebox/
+# # Add Jukebox folder
+# mkdir /jukebox
+# chmod 777 /jukebox
+# cp /var/www/Jukebox/setup/intro.mov /jukebox/
+# cp /var/www/Jukebox/setup/desktop.png /jukebox/
 
-# Add Music Folder
-mkdir /jukebox/music
-chmod 777 /jukebox/music
+# # Add Music Folder
+# mkdir /jukebox/music
+# chmod 777 /jukebox/music
+
+MUSIC_DIR=/var/lib/mpd/music
 
 # Add test file
-cp /var/www/Jukebox/setup/test.mp3 /jukebox/music/
+cp /var/www/Jukebox/setup/test.mp3 $MUSIC_DIR
 
 # Add desktop symlink
-ln -s /jukebox/music /home/pi/Desktop/Music
+ln -s $MUSIC_DIR /home/pi/Desktop/Music
+
+# Add External Symlinks
+ln -s /media/pi/MEDIA/ $MUSIC_DIR/media
+ln -s /media/pi/MUSIC/ $MUSIC_DIR/music
+ln -s /media/pi/JUKEBOX/ $MUSIC_DIR/jukebox
 
 # Add Video Playlist on Startup
 mkdir /home/pi/.config/autostart
